e73d71351b1a17b9c5951e8601860c2ceb0a93dd
Gianbelinche <39842759+gianbelinche@users.noreply.github.com>
2020-05-16 22:25:56 -0300

    Actualizo vecinos para que trabaje con vector unidimensional

--- main.c
+++ main.c
@@ -3,22 +3,29 @@
 #include <stdlib.h>
 
 //calcula la cantidad de vecinos de una celda
-unsigned int vecinos(unsigned char** a, unsigned int i, unsigned int j, unsigned int M, unsigned int N){
+unsigned int vecinos(unsigned char* a, unsigned int i, unsigned int j, unsigned int M, unsigned int N){
     unsigned int cant_vecinos = 0;
-    if (a[(i+1)%M][j] == 1) cant_vecinos++;
-    if (a[(i-1)%M][j] == 1) cant_vecinos++;
-    if (a[i][(j+1)%N] == 1) cant_vecinos++;
-    if (a[i][(j-1)%N] == 1) cant_vecinos++;
-    if (a[(i+1)%M][(j+1)%N] == 1) cant_vecinos++;
-    if (a[(i+1)%M][(j-1)%N] == 1) cant_vecinos++; //Se pueden optimizar mas si es necesario, por ahora esta bien asi.
-    if (a[(i-1)%M][(j+1)%N] == 1) cant_vecinos++;
-    if (a[(i-1)%M][(j-1)%N] == 1) cant_vecinos++;
+    if (a[((i+1)%M)*M + j] == 1) cant_vecinos++;
+    if (a[((i-1)%M)*M + j] == 1) cant_vecinos++;
+    if (a[i * M + ((j+1)%N)] == 1) cant_vecinos++;
+    if (a[i * M + ((j-1)%N)] == 1) cant_vecinos++;
+    if (a[((i+1)%M)*M + ((j+1)%N)] == 1) cant_vecinos++;
+    if (a[((i+1)%M)*M + ((j-1)%N)] == 1) cant_vecinos++; //Se pueden optimizar mas si es necesario, por ahora esta bien asi.
+    if (a[((i-1)%M)*M + ((j+1)%N)] == 1) cant_vecinos++;
+    if (a[((i-1)%M)*M + ((j-1)%N)] == 1) cant_vecinos++;
     return cant_vecinos;
 }
 
 //Devuelve 1 si la celda estara prendida o 0 si estara apagada
 char actualizar_celda(unsigned char** a, unsigned int i, unsigned int j, unsigned int M, unsigned int N){
-    unsigned int cant_vecinos = vecinos(a,i,j,M,N);
+    unsigned char a_formato_vector[M*N];
+    for (int i = 0; i < M;i++){
+        for (int j = 0; j < N; j++){
+            a_formato_vector[i*M+j] = a[i][j];
+        }
+    }
+
+    unsigned int cant_vecinos = vecinos(a_formato_vector,i,j,M,N);
     if (cant_vecinos == 3 && (!a[i][j])) return 1;
     if ((cant_vecinos == 2 || cant_vecinos == 3) && a[i][j]) return 1;
     return 0;
