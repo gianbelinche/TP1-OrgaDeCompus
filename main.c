9f09364e4d42973ca3582fdb2e0a653e3cc2cd85
JulianVentura <julianv6004@gmail.com>
2020-05-17 14:57:25 -0300

    Agrego parseador de archivo de ingreso

--- main.c
+++ main.c
@@ -70,3 +70,41 @@ int main(int argc, char* argv[]){
     }
     return 1;
 }
+
+
+/*
+
+//////////////////PARAMETROS DE INGRESO/////////////////////
+
+El programa debera ser capaz de tomar los siguientes argumentos de entrada:
+
+- conway -h --> Imprime un mensaje de ayuda que describe el uso del programa
+
+- conway i M N inputfile [-o outputprefix]
+
+Donde i representa la cantidad de estados consecutivos a imprimir
+
+M y N representan el tamanio de la matriz del juego
+
+inputfile es el archivo desde donde se levantaran las posiciones de las celdas prendidas
+
+outputprefix es el prefijo que se le debera agregar a los archivos .pbm
+que se impriman tras la ejecucion
+
+-o permite saber que el comando posterior es el outputprefix (util para parsear) 
+
+Ej:
+    conway 10 20 20 glider -0 estado
+
+    i = 10 --> 10 iteraciones impresas
+    M = N = 20 --> Tamanio de la matriz de 20x20
+    glider --> Se levanta la informacion del archivo de nombre "glider"
+    -o estado --> Los i archivos generados deberan nombrarse de la siguiente forma:
+            estado_1.pbm
+            estado_2.pbm
+                .
+                .
+                .
+            estado_i.pbm
+
+*/
---
+++ parseador_archivo.c
@@ -0,0 +1,88 @@
+#include "parseador_archivo.h"
+#include <string.h> //Para strtok
+#include <stdlib.h> //Para atoi
+#define FORMATO_LECTURA "r"
+#define DELIMITADOR " "
+#define TAM_BUFFER_LINEA 5
+#define LARGO_LINEA 3
+#define CELDA_ENCENDIDA 1
+#define EXITO 0
+#define ERROR -1
+//#define pos(x,y,M) (y*M + x) Se puede borrar si no lo usamos
+
+
+
+////////////Funciones privadas////////////////
+
+/*
+Toma la linea leida, la parsea y chequea que las coordenadas
+no queden fuera del rango de la matriz.
+Llena la matriz tablero con las posiciones leidas del archivo.
+En caso de fallo devuelve -1
+En caso de exito devuelve 0
+*/
+static int _parsear_linea(char* linea, 
+                          const unsigned int M, 
+                          const unsigned int N, 
+                          char** tablero){
+
+    char* ptr_x = strtok(linea, DELIMITADOR);
+    char* ptr_y = strtok(NULL, DELIMITADOR);
+    if (!ptr_y) return ERROR;
+    int x = atoi(ptr_x);
+    int y = atoi(ptr_y);
+    if (x < 0 || x >= N) return ERROR;
+    if (y < 0 || y >= M) return ERROR;
+    //tablero[pos(x, y, M)] = CELDA_ENCENDIDA;
+    tablero[y][x] = CELDA_ENCENDIDA;
+    return EXITO;
+}
+
+////////////Funciones publicas////////////////
+
+/*
+Inicializa la estructura parseador_archivo_t.
+En caso de exito devuelve 0 y debera llamarse a parseador_archivo_destruir() 
+para liberar los recursos utilizados al finalizar.
+En caso de error devuelve -1 y no sera necesario llamar a parseador_archivo_destruir().
+*/
+int parseador_archivo_crear(parseador_archivo_t *self, const char* nombre_archivo){
+    self->archivo = fopen(nombre_archivo, FORMATO_LECTURA);
+    if (!self->archivo) return ERROR;
+    return EXITO;
+}
+
+/*
+Destruye la estructura parseador_archivo_t, cerrando el archivo de entrada
+y liberando los recursos utilizados.
+Esta funcion no falla.
+*/
+void parseador_archivo_destruir(parseador_archivo_t *self){
+    //Siempre es bueno chequear que no cerremos un archivo no abierto
+    if (self->archivo){
+        fclose(self->archivo);
+        self->archivo = NULL;
+    }
+}
+
+/*
+Parsea el archivo de entrada y almacena los resultados leidos en la
+matriz tablero.
+En caso de que los valores leidos desde el archivo no respeten el rango
+de la matriz de tamanio MxN la funcion devolvera -1
+En caso de exito la funcion devolvera 0 y la matriz tablero contendra
+los valores de celdas leidos desde el archivo.
+*/
+int parseador_archivo_parsear(parseador_archivo_t *self, 
+                              const unsigned int M,
+                              const unsigned int N,
+                              char** tablero){
+    char linea[TAM_BUFFER_LINEA];
+    int estado = 0;
+    while(fgets(linea, TAM_BUFFER_LINEA, self->archivo) && estado == EXITO){
+        linea[strlen(linea)] = 0;
+        estado = _parsear_linea(linea, M, N, tablero);
+    }
+
+    return estado;
+}
