833c56cf9c2c9edfc01c7d842de585056d69225e
Gianbelinche <39842759+gianbelinche@users.noreply.github.com>
2020-05-25 16:44:07 -0300

    Agreog version facil de vecinos

--- life.c
+++ life.c
@@ -47,7 +47,7 @@ static int _inicializar_tablero(life_t *self){
     if (estado == ERROR) _liberar_tablero(self);
     return estado;
 }
-
+/*
 //calcula la cantidad de vecinos de una celda
 unsigned int vecinos(unsigned char* a, unsigned int x, unsigned int y, unsigned int fila, unsigned int columna){
     unsigned int cant_vecinos = 0;
@@ -64,8 +64,41 @@ unsigned int vecinos(unsigned char* a, unsigned int x, unsigned int y, unsigned 
     if (a[( ((i-1)%M) +M) %M *N + ( ((j+1)%N) +N) %N] == 1) cant_vecinos++;
     if (a[( ((i-1)%M) +M) %M *N + ( ((j-1)%N) +N) %N] == 1) cant_vecinos++;
     return cant_vecinos;
-}
+}*/
 
+unsigned int vecinos(unsigned char* a, unsigned int x, unsigned int y, unsigned int M, unsigned int N){
+    unsigned int cant_vecinos = 0;
+
+    unsigned int vecino_der_x = x+1;
+    if (x+1 >= N){
+        vecino_der_x = 0;
+    }
+    if (a[vecino_der_x * N + y] == 1) cant_vecinos++;
+
+    unsigned int vecino_izq_x = x-1;
+    if (x == 0){
+        vecino_izq_x = N-1;
+    }
+    if (a[vecino_izq_x * N + y] == 1) cant_vecinos++;
+
+    unsigned int vecino_arriba_y = y-1;
+    if (y == 0){
+        vecino_arriba_y = M -1;
+    }
+    if (a[x * N + vecino_arriba_y] == 1) cant_vecinos++;
+
+    unsigned int vecino_abajo_y = y+1;
+    if (y+1 >= M){
+        vecino_abajo_y = 0;
+    }
+    if (a[x*N+vecino_abajo_y]==1) cant_vecinos++;
+
+    if (a[vecino_der_x *N + vecino_abajo_y] == 1) cant_vecinos++;
+    if (a[vecino_der_x *N + vecino_arriba_y] == 1) cant_vecinos++;
+    if (a[vecino_izq_x *N + vecino_abajo_y] == 1) cant_vecinos++;
+    if (a[vecino_izq_x *N + vecino_arriba_y] == 1) cant_vecinos++;
+    return cant_vecinos;
+}
 //Devuelve 1 si la celda estara prendida o 0 si estara apagada
 char actualizar_celda(unsigned char** a, unsigned int i, unsigned int j, unsigned int M, unsigned int N){
     unsigned char a_formato_vector[M*N];
