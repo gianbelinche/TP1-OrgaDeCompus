f6e1a03461611e21e9bb229acd0f84c2386f33c6
Gianbelinche <39842759+gianbelinche@users.noreply.github.com>
2020-05-25 22:12:17 -0300

    Arreglo errores de ensamblado

--- life.c
+++ life.c
@@ -3,6 +3,7 @@
 #include "life.h"
 #include "parseador_archivo.h"
 #include "creador_archivo_pbm.h"
+#include "vecinos.h"
 #define EXITO 0
 #define ERROR -1
 #define ESCALA 10
@@ -66,39 +67,7 @@ unsigned int vecinos(unsigned char* a, unsigned int x, unsigned int y, unsigned 
     return cant_vecinos;
 }*/
 
-unsigned int vecinos(unsigned char* a, unsigned int x, unsigned int y, unsigned int M, unsigned int N){
-    unsigned int cant_vecinos = 0;
 
-    unsigned int vecino_der_x = x+1;
-    if (x+1 >= N){
-        vecino_der_x = 0;
-    }
-    if (a[vecino_der_x * N + y] == 1) cant_vecinos++;
-
-    unsigned int vecino_izq_x = x-1;
-    if (x == 0){
-        vecino_izq_x = N-1;
-    }
-    if (a[vecino_izq_x * N + y] == 1) cant_vecinos++;
-
-    unsigned int vecino_arriba_y = y-1;
-    if (y == 0){
-        vecino_arriba_y = M -1;
-    }
-    if (a[x * N + vecino_arriba_y] == 1) cant_vecinos++;
-
-    unsigned int vecino_abajo_y = y+1;
-    if (y+1 >= M){
-        vecino_abajo_y = 0;
-    }
-    if (a[x*N+vecino_abajo_y]==1) cant_vecinos++;
-
-    if (a[vecino_der_x *N + vecino_abajo_y] == 1) cant_vecinos++;
-    if (a[vecino_der_x *N + vecino_arriba_y] == 1) cant_vecinos++;
-    if (a[vecino_izq_x *N + vecino_abajo_y] == 1) cant_vecinos++;
-    if (a[vecino_izq_x *N + vecino_arriba_y] == 1) cant_vecinos++;
-    return cant_vecinos;
-}
 //Devuelve 1 si la celda estara prendida o 0 si estara apagada
 char actualizar_celda(unsigned char** a, unsigned int i, unsigned int j, unsigned int M, unsigned int N){
     unsigned char a_formato_vector[M*N];
---
+++ vecinos.h
@@ -0,0 +1,6 @@
+#ifndef HEADER_VECINOS
+#define HEADER_VECINOS
+
+unsigned int vecinos(unsigned char* a, unsigned int x, unsigned int y, unsigned int M, unsigned int N);   
+
+#endif
