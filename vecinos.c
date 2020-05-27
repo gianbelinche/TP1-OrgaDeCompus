afbf1f0204e48ede6e2fe79b38d345eea8267abd
Gianbelinche <39842759+gianbelinche@users.noreply.github.com>
2020-05-27 16:30:28 -0300

    Actualizo nombres erroneos

--- vecinos.c
+++ vecinos.c
@@ -3,33 +3,33 @@
 unsigned int vecinos(unsigned char* a, unsigned int x, unsigned int y, unsigned int M, unsigned int N){
     unsigned int cant_vecinos = 0;
 
-    unsigned int vecino_der_x = x+1;
+    unsigned int vecino_abajo_x = x+1;
     if (x+1 >= M){
-        vecino_der_x = 0;
+        vecino_abajo_x = 0;
     }
-    if (a[vecino_der_x * N + y] == 1) cant_vecinos++;
+    if (a[vecino_abajo_x * N + y] == 1) cant_vecinos++;
 
-    unsigned int vecino_izq_x = x-1;
+    unsigned int vecino_arriba_x = x-1;
     if (x == 0){
-        vecino_izq_x = M-1;
+        vecino_arriba_x = M-1;
     }
-    if (a[vecino_izq_x * N + y] == 1) cant_vecinos++;
+    if (a[vecino_arriba_x * N + y] == 1) cant_vecinos++;
 
-    unsigned int vecino_arriba_y = y-1;
+    unsigned int vecino_izq_y = y-1;
     if (y == 0){
-        vecino_arriba_y = N -1;
+        vecino_izq_y = N -1;
     }
-    if (a[x * N + vecino_arriba_y] == 1) cant_vecinos++;
+    if (a[x * N + vecino_izq_y] == 1) cant_vecinos++;
 
-    unsigned int vecino_abajo_y = y+1;
+    unsigned int vecino_der_y = y+1;
     if (y+1 >= N){
-        vecino_abajo_y = 0;
+        vecino_der_y = 0;
     }
-    if (a[x*N+vecino_abajo_y]==1) cant_vecinos++;
+    if (a[x*N+vecino_der_y]==1) cant_vecinos++;
 
-    if (a[vecino_der_x *N + vecino_abajo_y] == 1) cant_vecinos++;
-    if (a[vecino_der_x *N + vecino_arriba_y] == 1) cant_vecinos++;
-    if (a[vecino_izq_x *N + vecino_abajo_y] == 1) cant_vecinos++;
-    if (a[vecino_izq_x *N + vecino_arriba_y] == 1) cant_vecinos++;
+    if (a[vecino_abajo_x *N + vecino_der_y] == 1) cant_vecinos++;
+    if (a[vecino_abajo_x *N + vecino_izq_y] == 1) cant_vecinos++;
+    if (a[vecino_arriba_x *N + vecino_der_y] == 1) cant_vecinos++;
+    if (a[vecino_arriba_x *N + vecino_izq_y] == 1) cant_vecinos++;
     return cant_vecinos;
 }
